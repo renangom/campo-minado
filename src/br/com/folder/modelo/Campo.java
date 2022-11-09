4041b6c665a16ef14d15a38fb7198b8cb9cc81f8
Renan <renan23eduardo@gmail.com>
2022-11-09 16:36:43 -0300

    Create methods alternarMarcacao, abrir and vizinhancaSegura

--- src/br/com/folder/modelo/Campo.java
+++ src/br/com/folder/modelo/Campo.java
@@ -3,6 +3,8 @@ package br.com.folder.modelo;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.folder.excecao.ExplosaoException;
+
 public class Campo {
 	private final int linha;
 	private final int coluna;
@@ -36,5 +38,33 @@ public class Campo {
 		}else {
 			return false;
 		}
+	}
+	
+	void alternarMarcacao() {
+		if(!aberto) {
+			marcado = !marcado;
+		}
+	}
+	
+	boolean abrir() {
+		if(!aberto &&  !marcado) {
+			aberto = true;
+			
+			if(minado) {
+				throw new ExplosaoException();
+			}
+			
+			if(vizinhancaSegura()) {
+				vizinhos.forEach(v -> v.abrir());
+			}
+			
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	boolean vizinhancaSegura() {
+		return vizinhos.stream().noneMatch(v -> v.minado);
 	}
 }

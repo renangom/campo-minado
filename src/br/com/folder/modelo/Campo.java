becfaf85f58cc8536aa9b4c5735410495ca1a84a
Renan <renan23eduardo@gmail.com>
2022-11-09 15:39:49 -0300

    Create Campo Class

--- src/br/com/folder/modelo/Campo.java
+++ src/br/com/folder/modelo/Campo.java
@@ -1,5 +1,12 @@
 package br.com.folder.modelo;
 
 public class Campo {
-
+	private final int linha;
+	private final int coluna;
+	private boolean minado;
+	
+	Campo(int linha, int coluna){
+		this.linha = linha;
+		this.coluna = coluna;
+	}
 }

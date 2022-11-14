327f1e795f2cba3ee947e9c200b81e2746e089f4
Renan <renan23eduardo@gmail.com>
2022-11-14 14:52:44 -0300

    Create toString method for tabuleiro

---
+++ src/br/com/folder/cm/Aplicacao.java
@@ -0,0 +1,10 @@
+package br.com.folder.cm;
+
+import br.com.folder.modelo.Tabuleiro;
+
+public class Aplicacao {
+	public static void main(String[] args) {
+		Tabuleiro tabuleiro = new Tabuleiro(7,7,6);
+		System.out.println(tabuleiro);
+	}
+}
--- src/br/com/folder/modelo/Tabuleiro.java
+++ src/br/com/folder/modelo/Tabuleiro.java
@@ -61,4 +61,21 @@ public class Tabuleiro {
 		campos.stream().forEach(c -> c.reiniciar());
 		sortearMinas();
 	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		
+		int i = 0;
+		for(int l = 0; l < linhas; l++) {
+			for(int c = 0; c < colunas; c++) {
+				sb.append(" ");
+				sb.append(campos.get(i));
+				sb.append(" ");
+				i++;
+			}
+			sb.append("\n");
+		}
+		
+		return sb.toString();
+	}
 }
